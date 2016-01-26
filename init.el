3902141625fd1b72c9acf4493a6e7ef44fad57e3
ChristopheMees <christophe.mees@pearlchain.net>
2016-01-26 16:36:21 +0100

    Add some packages for javascript development

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
@@ -27,29 +28,38 @@
     ;; a filterable list of possible commands in the minibuffer
     ;; http://www.emacswiki.org/emacs/Smex
     smex
-    
+
     ;; Provides Emacs font-lock, indentation, and navigation for the Clojure programming language
     ;; Walkthroughs are available at clojure-doc.org and Clojure for the Brave and the True
     ;; https://github.com/clojure-emacs/clojure-mode
     clojure-mode
-    
+
     ;; Smartparens is minor mode for Emacs that deals with parens pairs and tries to be smart about it
     ;; https://github.com/Fuco1/smartparens
     smartparens
-    
+
     ;; rainbow-delimiters is a "rainbow parentheses"-like mode which highlights delimiters
     ;; such as parentheses, brackets or braces according to their depth.
     ;; https://github.com/Fanael/rainbow-delimiters
     rainbow-delimiters
-    
+
     ;; CIDER (formerly nrepl.el) is the Clojure Interactive Development Environment that Rocks for Emacs,
     ;; built on top of nREPL, the Clojure networked REPL server.
     ;; https://github.com/clojure-emacs/cider
     cider
-    
+
     ;; Company is a text completion framework for Emacs.
     ;; http://company-mode.github.io/
-    company))
+    company
+
+    ;; Improved JavaScript editing mode for GNU Emacs
+    ;; https://github.com/mooz/js2-mode
+    js2-mode
+
+    ;; Flycheck is a modern on-the-fly syntax checking extension for GNU Emacs,
+    ;; intended as replacement for the older Flymake extension which is part of GNU Emacs.
+    ;; http://www.flycheck.org/
+    flycheck))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -115,3 +125,48 @@
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
+
+;; Register js files for js2-mode
+(add-to-list 'auto-mode-alist ' ("\\.js$" . js2-mode))
+
+;; Add smartparents to js2-mode
+(add-hook 'js2-mode-hook #'smartparens-strict-mode)
+
+;; Set highlight level for js2-mode
+(setq js2-highlight-level 3)
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
+;;; runs eslint --fix on the current file after save
+;;; alpha quality -- use at your own risk
+
+(defun eslint-fix-file ()
+  (interactive)
+  (message "eslint --fixing the file" (buffer-file-name))
+  (shell-command (concat "eslint --fix " (buffer-file-name))))
+
+(defun eslint-fix-file-and-revert ()
+  (interactive)
+  (eslint-fix-file)
+  (revert-buffer t t))
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
